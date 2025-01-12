d816337b85748350ea758ae550cba044e4345f30
Tony Zimbinski <azimbin@gmail.com>
2025-01-12 02:00:11 -0600

    Update dependencies and Gradle wrapper for version compatibility, fixed entity rendering

--- src/main/java/cc/modlabs/resolutioncontrol/ResolutionControlMod.java
+++ src/main/java/cc/modlabs/resolutioncontrol/ResolutionControlMod.java
@@ -32,32 +32,32 @@ public class ResolutionControlMod implements ModInitializer {
 	public static final String MOD_NAME = "ResolutionControl++";
 
 	public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
-	
+
 	public static Identifier identifier(String path) {
 		return Identifier.of(MOD_ID, path);
 	}
-	
+
 	private static final MinecraftClient client = MinecraftClient.getInstance();
-	
+
 	private static ResolutionControlMod instance;
-	
+
 	public static ResolutionControlMod getInstance() {
 		return instance;
 	}
 
 	private static final String SCREENSHOT_PREFIX = "fb";
-	
+
 	private KeyBinding settingsKey;
 	private KeyBinding screenshotKey;
-	
+
 	private boolean shouldScale = false;
-	
+
 	@Nullable
 	private Framebuffer framebuffer;
 
 	@Nullable
 	private Framebuffer screenshotFrameBuffer;
-	
+
 	@Nullable
 	private Framebuffer clientFramebuffer;
 
@@ -75,11 +75,11 @@ public class ResolutionControlMod implements ModInitializer {
 	private int lastWidth;
 	private int lastHeight;
 	public boolean hasRun = false;
-	
+
 	@Override
 	public void onInitialize() {
 		instance = this;
-		
+
 		settingsKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
 				"key.resolutioncontrol.settings",
 				InputUtil.Type.KEYSYM,
@@ -148,12 +148,12 @@ public class ResolutionControlMod implements ModInitializer {
 				fb,
 				text -> client.player.sendMessage(text, false));
 	}
-	
+
 	public void setShouldScale(boolean shouldScale) {
 		if (shouldScale == this.shouldScale) return;
 
 //		if (getScaleFactor() == 1) return;
-		
+
 		Window window = getWindow();
 		if (framebuffer == null) {
 			this.shouldScale = true; // so we get the right dimensions
@@ -241,16 +241,16 @@ public class ResolutionControlMod implements ModInitializer {
 				getScreenshotWidth(), getScreenshotHeight()
 		);
 	}
-	
+
 	public float getScaleFactor() {
 		return Config.getInstance().scaleFactor;
 	}
-	
+
 	public void setScaleFactor(float scaleFactor) {
 		Config.getInstance().scaleFactor = scaleFactor;
-		
+
 		updateFramebufferSize();
-		
+
 		ConfigHandler.instance.saveConfig();
 	}
 
@@ -299,7 +299,7 @@ public class ResolutionControlMod implements ModInitializer {
 			setDownscaleAlgorithm(ScalingAlgorithm.NEAREST);
 		}
 	}
-	
+
 	public double getCurrentScaleFactor() {
 		return shouldScale ?
 				Config.getInstance().enableDynamicResolution ?
@@ -373,21 +373,21 @@ public class ResolutionControlMod implements ModInitializer {
 //		if (getWindow().getScaledHeight() == lastWidth
 //				|| getWindow().getScaledHeight() == lastHeight)
 //		{
-			updateFramebufferSize();
+		updateFramebufferSize();
 
-			lastWidth = getWindow().getScaledHeight();
-			lastHeight = getWindow().getScaledHeight();
+		lastWidth = getWindow().getScaledHeight();
+		lastHeight = getWindow().getScaledHeight();
 //		}
 
 
 	}
-	
+
 	public void updateFramebufferSize() {
 		if (framebuffer == null)
 			return;
 
 		resize(framebuffer);
-		resize(client.worldRenderer.getEntityOutlinesFramebuffer());
+		resize(client.worldRenderer.entityOutlineFramebuffer);
 		resizeMinecraftFramebuffers();
 
 		calculateSize();
@@ -405,7 +405,7 @@ public class ResolutionControlMod implements ModInitializer {
 		// Framebuffer uses color (4 x 8 = 32 bit int) and depth (32 bit float)
 		estimatedMemory = (long) currentWidth * currentHeight * 8;
 	}
-	
+
 	public void resize(@Nullable Framebuffer framebuffer) {
 		if (framebuffer == null) return;
 
@@ -425,11 +425,11 @@ public class ResolutionControlMod implements ModInitializer {
 		}
 		shouldScale = prev;
 	}
-	
+
 	private Window getWindow() {
 		return client.getWindow();
 	}
-	
+
 	private void setClientFramebuffer(Framebuffer framebuffer) {
 		client.framebuffer = framebuffer;
 	}
